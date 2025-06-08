962ad2c322d6021ea7045a1f5b8056a5db74de79
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-06-08 13:49:37 +0700

    update files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import { Routes, Route, Link } from "react-router-dom";
-import Home from './pages/index';
-import About from './pages/About';
-import Shop from './pages/Shop';
-import Contact from './pages/Contact';
-import Detail from './pages/Detail';
-
+import Home from './pages/index/index';
+import About from './pages/index/About';
+import Shop from './pages/index/Shop';
+import Contact from './pages/index/Contact';
+import Detail from './pages/index/Detail';
+import Login from './pages/auth/login';
+import Register from './pages/auth/register';
+// import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+    </Routes>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom";
 import './App.css'
+import { UserProvider } from './context/UserContext.jsx';
+import { CartProvider } from './context/CartContext.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
-      <App />
+      <UserProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </UserProvider>
     </Router>
   </StrictMode>,
 )
