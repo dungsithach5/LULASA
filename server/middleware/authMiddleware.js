f7989c2dc7706eef05b7e1eddf977340e8070a1e
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-06-08 13:41:07 +0700

    auth middleware for JWT

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.JWT_SECRET || 'my-secret-key';
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) {
+      console.error('JWT verification error:', err);
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+}
+
+module.exports = authMiddleware;
