57307c48213970f33bb8bb427621cd2de6239492
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-06-08 13:43:13 +0700

    create seeders

--- server/seeders/20250531113135-productSeeders.js
+++ server/seeders/20250531113135-productSeeders.js
@@ -5,12 +5,12 @@ module.exports = {
   async up (queryInterface) {
     await queryInterface.bulkInsert('Products', [
       {
-        name: 'Product 1',
+        name: 'Syzygium Soap ',
         short_desc: 'Experience the tranquility of lavender fields with our handcrafted soap. Made with pure lavender essential oil and dried lavender buds, this soap provides a gentle cleansing experience while calming your senses. The natural antibacterial properties of lavender help purify your skin, leaving it feeling refreshed and balanced.',
-        long_desc:' This is a sample product description for Sanyam Goyal.',
-        ingredients: 'Ingredient1, Ingredient2',
+        long_desc:'Awaken your skin with the pure purity of Vietnamese Vối leaves. Blending the essence of fresh Vối leaves – a native plant famous for its antibacterial and anti-inflammatory properties – with coconut oil and organic shea butter, Syzygium Soap creates a natural, smooth foam that gently cleanses while still retaining moisture for the skin. The gentle, fresh scent of Vối leaves brings a feeling of relaxation and refreshment after each use.',
+        ingredients: 'Vietnamese Syzygium nervosum extract, organic shea butter & coconut oil.',
         price: 19.99,
-        main_image_url: 'src/assets/img/Product/product1.png',
+        main_image_url: '/public/img/Product/product4.png',
         createdAt: new Date(),
         updatedAt: new Date()
       },
---
+++ server/seeders/20250607064611-userSeeders.js
@@ -0,0 +1,21 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.bulkInsert('Users', [
+      {
+        name: 'Sanyam Goyal',
+        email: 'dungsithach13@gmail.com',
+        password: await bcrypt.hash('123', 10),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ], {});
+  },
+
+  async down (queryInterface) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
