75c9f33070e2fb63342fe5b062a153b081d7d6a0
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-06-18 18:09:23 +0700

    API from Railway

--- src/service/api/productApi.js
+++ src/service/api/productApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchProducts = async () => {
   try {
-    const response = await axios.get('http://localhost:3000/api/products');
+    const response = await axios.get('https://lulasa-production.up.railway.app/api/products');
     return response.data;
   } catch (error) {
     console.error('Error fetching products:', error);
@@ -12,7 +12,7 @@ export const fetchProducts = async () => {
 
 export const fetchProductById = async (id) => {
   try {
-    const response = await axios.get(`${'http://localhost:3000/api/products'}/${id}`);
+    const response = await axios.get(`${'https://lulasa-production.up.railway.app/api/products'}/${id}`);
     return response.data;
   } catch (error) {
     console.error(`Error fetching product with ID ${id}:`, error);
