7419f6bee8fc4656a704a89f734a6c13bc670837
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-06-18 18:16:32 +0700

    update server to deploy vercel

--- server/app.js
+++ server/app.js
@@ -14,11 +14,10 @@ const PORT = process.env.PORT || 3000;
 // app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Middleware
-app.use(cors());
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-//   credentials: true,  
-// }));
+app.use(cors({
+  origin: 'https://lulasa.vercel.app/',
+  credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
