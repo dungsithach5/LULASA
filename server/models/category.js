f5a19bab69af2c835ee73d6f5dd44968a819d44c
dungsithach5 <159418327+dungsithach5@users.noreply.github.com>
2025-06-20 22:03:51 +0700

    update file

--- server/models/category.js
+++ server/models/category.js
@@ -16,7 +16,6 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Category.init({
-    id: DataTypes.INTEGER,
     name: DataTypes.STRING
   }, {
     sequelize,
--- server/seeders/20250620131702-categorySeeders.js
+++ server/seeders/20250620131702-categorySeeders.js
@@ -5,25 +5,21 @@ module.exports = {
   async up (queryInterface) {
     await queryInterface.bulkInsert('Categories', [
       {
-        id: 1,
         name: 'Syzygium',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        id: 2,
         name: 'Camellia',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        id: 3,
         name: 'P.betle',
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        id: 4,
         name: 'Herbal',
         createdAt: new Date(),
         updatedAt: new Date()
